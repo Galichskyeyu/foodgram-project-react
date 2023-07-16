e3bb1ef08cdd7faf8c5c862991a5997b7b269707
Emin Galichsky <galichskyeyu1992@gmail.com>
2023-07-16 23:19:09 +0300

    Update

---
+++ backend/recipes/management/commands/load_tags.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+from recipes.models import Tag
+
+
+class Command(BaseCommand):
+    help = 'Загрузка тэгов'
+
+    def handle(self, *args, **kwargs):
+        data = [
+            {'name': 'Завтрак', 'color': '#00FF00', 'slug': 'breakfast'},
+            {'name': 'Обед', 'color': '#FF0000', 'slug': 'dinner'},
+            {'name': 'Ужин', 'color': '#0000FF', 'slug': 'supper'}
+            ]
+        Tag.objects.bulk_create(Tag(**tag) for tag in data)
+        self.stdout.write(self.style.SUCCESS('Тэги успешно загружены.'))
