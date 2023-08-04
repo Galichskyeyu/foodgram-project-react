e11160185a99baafdb6f47594dc96f780fe74a13
Emin Galichsky <galichskyeyu1992@gmail.com>
2023-08-04 09:38:44 +0400

    Update

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,7 +15,7 @@ from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from recipes.models import (FavoriteRecipe, Ingredient, Recipe, ShoppingCart,
-                            Subscribe, Tag)
+                            Tag)
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
